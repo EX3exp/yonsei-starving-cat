ae7cc295c17ebc5443b5ffb61dc166673a3bd939
EX3 <inthe_6788@naver.com>
2024-05-29 20:27:48 +0900

    added classes

--- StarvingCat/StarvingCat/main.cpp
+++ StarvingCat/StarvingCat/main.cpp
@@ -1,4 +1,10 @@
-// windows가 아닐 경우 파일경로 구분 문자열을 "\\"에서 '/'로 바꿔야 돌아갑니다
+// windows가 아닐 경우 파일경로 구분 문자열을 "\\"에서 '/'로 바꿔야 돌아갑니다 -- learnopengl, utils 내부 관련 헤더도 전부 수정해야 함 
+// (main.cpp 이외엔 딱히 수정하진 않으셔도 되고 교수님께서 올려 주신 헤더파일들 그대로 사용하시면 됩니다)
+// 
+// <디버그 폴더 내에 리소스 파일을 붙여넣어야 할 경우>:
+// (수동으로 복붙하지 마시고)
+// 프로젝트 속성 >> 빌드 후 이벤트
+// 명령줄에 `copy "$(ProjectDir)<추가할 파일>" "$(OutDir)" ` 을 추가해 주세요
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -13,6 +19,7 @@
 #include <learnopengl/model_animation.h>
 #include <iostream>
 
+
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 #include <unordered_map>
@@ -20,6 +27,13 @@
 // Source and Data directories
 string sourceDirStr = "C:\\Users\\inthe\\Downloads\\yonsei-starving-cat\\StarvingCat\\StarvingCat";
 string modelDirStr = "C:\\Users\\inthe\\Downloads\\yonsei-starving-cat\\data";
+
+enum Scene
+{ // 현재 렌더되는 화면을 구분하는 enum -- 씬 전환 시  
+    cat_init, // 게임 기동 시 맨 처음 화면
+    cat_eat_right,
+    cat_eat_left
+};
 
 // FUNCTION PROTOTYPES
 GLFWwindow *glAllInit();
@@ -29,10 +43,21 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
 
 // GLOBAL VARIABLES
-const double MAX_FRAMERATE_LIMIT = 1.0 / 60.0;
+const double MAX_FRAMERATE_LIMIT = 1.0 / 60.0; // 현재 프레임레이트 -- 기본값은 60프레임
 
 const unsigned int SCR_WIDTH = 1280;
 const unsigned int SCR_HEIGHT = 720;
+
+const int MAX_STAGE = 10;
+const int MIN_STAGE = 1;
+
+// share variables
+static int stage; // stage, 1~10
+static float catSize; // stage가 늘 때마다 증가
+string foodRight = "none"; // 오른쪽 밥그릇에 있는 food, 아무것도 없을 때엔 "none"
+string foodLeft = "none"; // 왼쪽 밥그릇에 있는 food, 아무것도 없을 때엔 "none"
+Scene currentScene = cat_init; // 씬 전환 시 이 변수를 변경해 주세요
+
 GLFWwindow *mainWindow = NULL;
 
 // camera
@@ -46,41 +71,47 @@ double deltaTime = 0.0;
 double lastUpdateTime = 0.0;
 double lastFrameTime = 0.0;
 
-class AnimatedObj3D 
-{ // 애니메이팅되는 3d 모델 오브젝트들은 모두 이 클래스를 상속받아서 사용
+class Obj3D
+{ // 3d 모델 오브젝트들은 모두 이 클래스를 상속받아서 사용 - 애니메이팅 적용되는 오브젝트의 경우 AnimatedObj3D 상속해야 함
 public:
-    AnimatedObj3D(string name, string modelPath, string vertexShaderPath, string fragShaderPath) : modelPath(modelPath), model(modelPath), shader(vertexShaderPath.c_str(), fragShaderPath.c_str()) 
+    Obj3D(string name, string modelPath, string vertexShaderPath, string fragShaderPath) : modelPath(modelPath), model(modelPath), shader(vertexShaderPath.c_str(), fragShaderPath.c_str())
     {
+        cout << "[Obj3D] '" + name + "' object created" << endl;
     }
 
-    void draw() 
-    {
-        animator.UpdateAnimation(deltaTime); // update animation
-        shader.use();
+    ~Obj3D() {
+        cout << "[Obj3D] '" + name + "' object deleted" << endl;
+    }
 
+    void draw()
+    {
+        shader.use();
+        updateAnimation();
         // view/projection transformations
         projectionMatrix = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         viewMatrix = camera.GetViewMatrix();
         shader.setMat4("projection", projectionMatrix);
         shader.setMat4("view", viewMatrix);
 
-        
-
-        transformsMatrixes = animator.GetFinalBoneMatrices();
-        for (int i = 0; i < transformsMatrixes.size(); ++i)
-            shader.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transformsMatrixes[i]);
+        sendAnimationToShader();
 
         shader.setMat4("model", modelMatrix);
         model.Draw(shader);
 
-        
+    }
+    virtual void updateAnimation() 
+    {
     }
 
-    void translate(float x=0.f, float y=-0.4f, float z=0.f) {
+    virtual void sendAnimationToShader() 
+    {
+    }
+    
+    void translate(float x = 0.f, float y = -0.4f, float z = 0.f) {
         modelMatrix = glm::translate(modelMatrix, glm::vec3(x, y, z));
     }
 
-    void scale(float x=.5f, float y=.5f, float z=.5f) {
+    void scale(float x = .5f, float y = .5f, float z = .5f) {
         modelMatrix = glm::scale(modelMatrix, glm::vec3(x, y, z));
     }
 protected:
@@ -89,16 +120,38 @@ protected:
     string modelPath;
     Shader shader;
 
-    Animation anim{ modelPath, &model };
-    Animator animator{&anim};
-    
-
     glm::mat4 projectionMatrix = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
     glm::mat4 viewMatrix = camera.GetViewMatrix();
     glm::mat4 modelMatrix = glm::mat4(1.0f);
 
+    Animation anim{ modelPath, &model };
+    Animator animator{ &anim };
+
     std::vector<glm::mat4> transformsMatrixes = animator.GetFinalBoneMatrices();
 };
+
+class AnimatedObj3D : public Obj3D
+{ // 애니메이팅되는 3d 모델 오브젝트들은 모두 이 클래스를 상속받아서 사용
+public:
+    AnimatedObj3D(string name, string modelPath, string vertexShaderPath, string fragShaderPath) : Obj3D(name, modelPath, vertexShaderPath, fragShaderPath)
+    {
+        
+    }
+
+
+    void updateAnimation() override
+    {
+        animator.UpdateAnimation(deltaTime); // update animation
+    }
+
+    void sendAnimationToShader() override {
+        transformsMatrixes = animator.GetFinalBoneMatrices();
+        for (int i = 0; i < transformsMatrixes.size(); ++i)
+            shader.setMat4("finalBonesMatrices[" + std::to_string(i) + "]", transformsMatrixes[i]);
+    }
+};
+
+
 
 class Cat : public AnimatedObj3D
 { // 고양이
